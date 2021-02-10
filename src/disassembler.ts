f5b3e0cccf24c32e438be7037d230a9f8ca1f3c0
Dimitris Karagiannis <mitch.karajohn@gmail.com>
2021-02-10 07:39:59 +0200

    Add some more opcodes and small improvements

--- src/disassembler.ts
+++ src/disassembler.ts
@@ -12,7 +12,7 @@ type DisassembledInstructionToken = {
 };
 
 function generateDisassembledInstructionToken(
-  opcodeToken: OpcodeToken,
+  opcodeToken: OpcodeToken | undefined,
   bytecode: Uint8Array,
   index: number
 ): DisassembledInstructionToken {
@@ -25,36 +25,24 @@ function generateDisassembledInstructionToken(
 
   switch (opcodeToken.operand) {
     case 'a8':
-      return {
-        position: `$${zeroPadHalfByte(index)}`,
-        code: opcodeToken.instruction.replace(
-          'a8',
-          `$${convertNumberToHexString(bytecode[index + 1])}`
-        ),
-      };
     case 'r8':
-      return {
-        position: `$${zeroPadHalfByte(index)}`,
-        code: opcodeToken.instruction.replace(
-          'r8',
-          `$${convertNumberToHexString(bytecode[index + 1])}`
-        ),
-      };
     case 'd8':
       return {
         position: `$${zeroPadHalfByte(index)}`,
         code: opcodeToken.instruction.replace(
-          'd8',
+          '${OPERAND}',
           `$${convertNumberToHexString(bytecode[index + 1])}`
         ),
       };
+    case 'a16':
     case 'd16':
       return {
         position: `$${zeroPadHalfByte(index)}`,
         code: opcodeToken.instruction.replace(
-          'd16',
-          `$${convertNumberToHexString(bytecode[index + 2])}${convertNumberToHexString(
-            bytecode[index + 1]
+          '${OPERAND}',
+          `$${zeroPadHalfByte(
+            convertNumberToHexString(bytecode[index + 2]) +
+              convertNumberToHexString(bytecode[index + 1])
           )}`
         ),
       };
@@ -73,24 +61,28 @@ function disassemble(bytecode: Uint8Array) {
     const opcode = bytecode[i];
     const opcodeToken = opcodes[opcode];
 
-    switch (opcode) {
-      // opcode 0xcb references opcodes from the CB opcode table so it needs special handling
-      case convertHexStringToDecimalNumber('0xcb'): {
-        const referencedObOpcodeToken = cbOpcodes[bytecode[i + opcodeToken.length]];
-        disassembledCode.push(
-          generateDisassembledInstructionToken(referencedObOpcodeToken, bytecode, i)
-        );
-        if (referencedObOpcodeToken) {
-          i += referencedObOpcodeToken.length;
-        } else {
-          i += opcodeToken.length;
+    if (!opcodeToken) {
+      disassembledCode.push(generateDisassembledInstructionToken(opcodeToken, bytecode, i));
+      i++;
+    } else {
+      switch (opcode) {
+        // opcode 0xcb references opcodes from the CB opcode table so it needs special handling
+        case convertHexStringToDecimalNumber('0xcb'): {
+          const referencedObOpcodeToken = cbOpcodes[bytecode[i + opcodeToken.length]];
+          disassembledCode.push(
+            generateDisassembledInstructionToken(referencedObOpcodeToken, bytecode, i)
+          );
+          if (referencedObOpcodeToken) {
+            i += referencedObOpcodeToken.length;
+          } else {
+            i += opcodeToken.length;
+          }
+          break;
         }
-        break;
+        default:
+          disassembledCode.push(generateDisassembledInstructionToken(opcodeToken, bytecode, i));
+          i += opcodeToken.length;
       }
-      default:
-        // opcodeToken can be undefined here
-        disassembledCode.push(generateDisassembledInstructionToken(opcodeToken, bytecode, i));
-        i += opcodeToken?.length || 1;
     }
   }
 
--- src/opcodes.ts
+++ src/opcodes.ts
@@ -16,7 +16,7 @@ type Registers =
   | 'SP'
   | 'PC';
 
-type FlagState = 'flip' | 'set' | 'reset';
+type FlagState = 'depends' | 'set' | 'reset';
 
 type Flags = {
   Z?: FlagState;
@@ -56,22 +56,40 @@ function generateOpcodeToken(
   };
 }
 
-const opcodes: OpcodeToken[] = new Array(256);
-const cbOpcodes: OpcodeToken[] = new Array(256);
+const opcodes: (OpcodeToken | undefined)[] = new Array(256);
+const cbOpcodes: (OpcodeToken | undefined)[] = new Array(256);
 
 opcodes[0x00] = generateOpcodeToken('NOP', 1, 4);
-opcodes[0x0e] = generateOpcodeToken('LD C,d8', 2, 8, undefined, 'd8');
-opcodes[0x20] = generateOpcodeToken('JR NZ,SIGNED r8', 2, { high: 12, low: 8 }, undefined, 'r8');
-opcodes[0x21] = generateOpcodeToken('LD HL,d16', 3, 12, undefined, 'd16');
-opcodes[0x31] = generateOpcodeToken('LD SP,d16', 3, 12, undefined, 'd16');
+opcodes[0x0e] = generateOpcodeToken('LD C,${OPERAND}', 2, 8, undefined, 'd8');
+opcodes[0x11] = generateOpcodeToken('LD DE,${OPERAND}', 3, 12, undefined, 'd16');
+opcodes[0x13] = generateOpcodeToken('INC DE', 1, 8);
+opcodes[0x1a] = generateOpcodeToken('LD A,(DE)', 1, 8, undefined);
+opcodes[0x20] = generateOpcodeToken(
+  'JR NZ,SIGNED ${OPERAND}',
+  2,
+  { high: 12, low: 8 },
+  undefined,
+  'r8'
+);
+opcodes[0x21] = generateOpcodeToken('LD HL,${OPERAND}', 3, 12, undefined, 'd16');
+opcodes[0x31] = generateOpcodeToken('LD SP,${OPERAND}', 3, 12, undefined, 'd16');
 opcodes[0x32] = generateOpcodeToken('LD (HL-),A', 1, 8);
-opcodes[0x3e] = generateOpcodeToken('LD A,d8', 2, 8, undefined, 'd8');
+opcodes[0x3e] = generateOpcodeToken('LD A,${OPERAND}', 2, 8, undefined, 'd8');
 opcodes[0x77] = generateOpcodeToken('LD (HL),A', 1, 8);
-opcodes[0xaf] = generateOpcodeToken('XOR A', 1, 4, { Z: 'flip' });
+opcodes[0x7b] = generateOpcodeToken('LD A,E', 1, 4);
+opcodes[0xaf] = generateOpcodeToken('XOR A', 1, 4, { Z: 'depends' });
 opcodes[0xcb] = generateOpcodeToken('PREFIX CB', 1, 4);
-opcodes[0xe0] = generateOpcodeToken('LD ($FF00+a8),A', 2, 12, undefined, 'a8');
+opcodes[0xcd] = generateOpcodeToken('CALL ${OPERAND}', 3, 24, undefined, 'a16');
+opcodes[0xe0] = generateOpcodeToken('LD ($FF00+${OPERAND}),A', 2, 12, undefined, 'a8');
 opcodes[0xe2] = generateOpcodeToken('LD (C),A', 2, 8);
+opcodes[0xfe] = generateOpcodeToken(
+  'CP ${OPERAND}',
+  2,
+  8,
+  { Z: 'depends', N: 'set', H: 'depends', C: 'depends' },
+  'd8'
+);
 
-cbOpcodes[0x7c] = generateOpcodeToken('BIT 7,H', 2, 8, { Z: 'flip', N: 'reset', H: 'set' });
+cbOpcodes[0x7c] = generateOpcodeToken('BIT 7,H', 2, 8, { Z: 'depends', N: 'reset', H: 'set' });
 
 export { opcodes, cbOpcodes };
