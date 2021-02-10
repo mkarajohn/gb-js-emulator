e37fc5752d9b7b581349333053e48c50fb1efcb5
Dimitris Karagiannis <mitch.karajohn@gmail.com>
2021-02-10 21:23:34 +0200

    Improve zeropad function

--- src/utils.ts
+++ src/utils.ts
@@ -6,21 +6,17 @@ export function convertHexStringToDecimalNumber(s: string) {
   return parseInt(s, 16);
 }
 
-export function zeroPadHalfByte(n: number | string) {
+export function zeroPad(n: number | string, paddedLength: number = 4) {
   const numberAsHexString = typeof n === 'number' ? convertNumberToHexString(n) : n;
 
-  switch (numberAsHexString.length) {
-    case 0:
-      return '0000';
-    case 1:
-      return `000${numberAsHexString}`;
-    case 2:
-      return `00${numberAsHexString}`;
-    case 3:
-      return `0${numberAsHexString}`;
-    case 4:
-      return numberAsHexString;
-    default:
-      throw new Error('Invalid number passed to zeropad');
+  function doPadding(paddedLength: number, hexString: string) {
+    let pad = '';
+    while (pad.length < paddedLength - hexString.length) {
+      pad += '0';
+    }
+
+    return `${pad}${hexString}`;
   }
+
+  return doPadding(paddedLength, numberAsHexString);
 }
